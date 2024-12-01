05243b4421f74d658fa42de279ca4cd0d92e0f76
nrebei2 <rebein002@gmail.com>
2024-12-01 16:44:10 -0500

    invalid command

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,52 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::str::FromStr;
+
+enum Command {
+    Echo,
+    Cd,
+    Empty,
+}
+
+impl FromStr for Command {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut blocks = s.split_whitespace();
+
+        match blocks.next() {
+            None => Ok(Self::Empty),
+            Some(comm) => match &comm.to_ascii_lowercase()[..] {
+                "echo" => Ok(Self::Echo),
+                "cd" => Ok(Self::Cd),
+                _ => Err(format!("invalid_command: {} not found", comm)),
+            },
+        }
+    }
+}
+
+fn print(s: &str) {
+    print!("{s}");
+    io::stdout().flush().unwrap();
+}
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+
+    loop {
+        print("$ ");
+        stdin.read_line(&mut input).unwrap();
+
+        match Command::from_str(&input) {
+            Ok(comm) => {
+                todo!()
+            }
+            Err(e) => {
+                println!("{e}");
+            }
+        }
+
+        input.clear();
+    }
 }
